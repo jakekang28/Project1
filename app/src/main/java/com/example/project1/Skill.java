cc6f38713af9ad3bba357b2ccb82d2367bfb0b82
jakekang28 <jakekang28@kaist.ac.kr>
2022-07-04 23:10:08 +0900

    battlefield implementing

--- app/src/main/java/com/example/project1/Skill.java
+++ app/src/main/java/com/example/project1/Skill.java
@@ -3,6 +3,35 @@ package com.example.project1;
 import android.graphics.drawable.Drawable;
 
 public class Skill {
-    
-
+    String type;
+    double prob;
+    int count;
+    int power;
+    public String getType(Skill skill){
+        return skill.type;
+    }
+    public double getProb(Skill skill){
+        return skill.prob;
+    }
+    public int getCount(Skill skill){
+        return skill.count;
+    }
+    public int getPower(Skill skill){
+        return skill.power;
+    }
+    public void setType(String type){
+        this.type=type;
+    }
+    public void setProb(double prob){
+        this.prob=prob;
+    }
+    public void setCount(int count){
+        this.count=count;
+    }
+    public void setPower(int power){
+        this.power=power;
+    }
 }
+
+
+
